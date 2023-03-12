955ca11283f6425c01821d71059ea356b2b5f12a
RilCritch <rilcritch@gmail.com>
2023-03-11 19:03:56 -0600

    update

--- .ril_funcs.sh
+++ .ril_funcs.sh
@@ -21,14 +21,25 @@ pl()
 ## - push bash config files to github ----------------------------#
 pushbc()
 {
-	bcgit
-	git add . && git commit -m "update" && git push --all
+	bcgit && putil
 }
 
 ## - push note files to github -----------------------------------#
 pushnt()
 {
-	ntgit
+	ntgit && putil
+}
+
+## - push mis config to github
+pushmc()
+{
+	mcgit && putil
+}
+
+## - utility for push fncs ---------------------------------------#
+#### - Send it a location
+putil()
+{
 	git add . && git commit -m "update" && git push --all
 }
 
